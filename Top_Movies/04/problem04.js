40907a8ec20ecd412a04f9c879ff777bfcb9ef19
Kenson Johnson <kenson.johnson@outlook.com>
2022-12-07 09:57:42 -0600

    Setting up new questions

---
+++ Top_Movies/04/problem04.js
@@ -0,0 +1,49 @@
+import { readFileSync } from "fs";
+
+const fileReader = function (filepath) {
+  return readFileSync(filepath, "utf8").toString();
+};
+
+function csvToArray(str, delimiter = ",") {
+  // get the first row as header data
+  const headers = str.slice(0, str.indexOf("\n")).trim().split(delimiter);
+
+  //put all of the remaining rows into an array
+  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+  // Map the rows
+  const arr = rows.map((row) => {
+    // split values from each row into an array
+    const values = row.trim().split(delimiter);
+    // use headers.reduce to create an object
+    const el = headers.reduce((object, header, index) => {
+      // if there is a space between words, we need to format like a property name
+      if (header.includes(" ")) {
+        const seperateWords = header.split(" ");
+        // this can only handle a single space, for now
+        let formated = seperateWords[0].toLowerCase() + seperateWords[1];
+        object[formated] = values[index];
+      } else {
+        // object properties derived from headers:values
+        object[header.toLowerCase()] = values[index];
+      }
+
+      return object;
+    }, {});
+    // the object passed as an element of the array
+    return el;
+  });
+
+  // return the array
+  return arr;
+}
+
+const question =
+  "What is the earliest year on this list, and what were the films from that year?";
+
+const movies = csvToArray(fileReader("../top_movies.csv"));
+
+// Code here
+
+console.log(question);
+console.log(movies);
