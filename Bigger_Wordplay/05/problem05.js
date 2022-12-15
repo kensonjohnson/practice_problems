a175d8d6b32ac9786af8b079d1b5f4f90f51a2ce
Kenson Johnson <kenson.johnson@outlook.com>
2022-12-14 21:49:22 -0600

    Finishing Problems

--- Bigger_Wordplay/05/problem05.js
+++ Bigger_Wordplay/05/problem05.js
@@ -1,13 +1,56 @@
 import readFile from "../../CustomParser.js";
-
-const scrabbleWords = readFile("../sowpods.txt");
+console.time("Total Time");
+const scrabbleWords = readFile("../sowpods.txt").sort((a, b) => {
+  return a.length - b.length;
+});
+const words = new Set(scrabbleWords);
 
 const question =
   "What are all of the compound words? These are words made up of 2 smaller words.";
 const example =
-  "For example: \n“SNOWMAN” is a compound word made from “SNOW” and “MAN”, and “BEACHBALL” is a compound word made from “BEACH” and “BALL”.\n";
+  "For example: \n - “SNOWMAN” is a compound word made from “SNOW” and “MAN”, and “BEACHBALL” is a compound word made from “BEACH” and “BALL”.\n";
+const matches = new Set();
+for (let i = 0; i < scrabbleWords.length; i++) {
+  if (i % 50 === 0) {
+    console.log(i);
+    console.log(matches.size);
+  }
+  const firstWord = scrabbleWords[i];
+  if (!firstWord || firstWord.length < 3) {
+    continue;
+  }
+  if (firstWord.length > 13) {
+    break;
+  }
+  if (matches.has(firstWord)) {
+    continue;
+  }
+  const minLength = firstWord.length + 2;
+  if (i % 50 === 0) {
+    console.time("Inner Loop");
+  }
+  scrabbleWords.forEach((secondWord) => {
+    if (secondWord.length < firstWord.length) {
+      return;
+    }
+    const biggerWord = firstWord + secondWord;
+    if (biggerWord.length > 15 || biggerWord.length < minLength) {
+      return;
+    }
+    const biggerWord2 = secondWord + firstWord;
+    if (words.has(biggerWord)) {
+      matches.add(biggerWord);
+    }
+    if (words.has(biggerWord2)) {
+      matches.add(biggerWord2);
+    }
+  });
+  if (i % 50 === 0) {
+    console.timeEnd("Inner Loop");
+  }
+}
 
-  // code here
-
-console.log(question + "\n");
+console.log(question);
 console.log(example);
+// console.log(matches);
+console.timeEnd("Total Time");
