ada8e41fc8170312ef6b55136fdb91dcbae43c2a
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-08 13:32:55 -0600

    Setting up

---
+++ Algorithms/Dijkstra.js
@@ -0,0 +1,21 @@
+/*
+ *
+ */
+
+const sampleGraph = [
+  [0, 1, 2],
+  [1, 1, 2],
+  [3, 1, 0],
+];
+
+// establish priority queue
+// place starting node in queue
+
+// establish a place to hold finished nodes
+
+// establish a place to hold where we've been
+
+// visit node with min distance in queue
+// load all of the children into the queue and update their distances
+// if new distance is lower than previous distance (from another point), then replace it
+// Once all distances are set, remove current point and store it for backtracking later
