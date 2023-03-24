3f6c1dd437eabb82ea848d561d195644efd1df83
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-24 11:56:53 -0500

    Cleaning code

---
+++ Mock_Interviews/depth_sum.js
@@ -0,0 +1,34 @@
+// given an array sum all integers in the array
+// each element has either an integer or an array of the same structure
+// if an integer is in a nested array, multiply that by the depth before summing
+
+const data = [1, [1, [1]], 5, [3], [2]];
+const empty = [];
+const allNums = [1, 2, 3, 4, 5];
+
+function depthSum(element, level = 1) {
+  let integerSum = 0;
+  // filter out the nested arrays into a sub-array
+  element.forEach((element) => {
+    if (typeof element === "number") {
+      return (integerSum += element * level);
+    }
+    integerSum += depthSum(element, level + 1);
+  });
+
+  return integerSum;
+}
+
+function depthSumWithReducer(array, level = 1) {
+  return array.reduce((sum, element) => {
+    if (typeof element === "number") return sum + element * level;
+    return sum + depthSumWithReducer(element, level + 1);
+  }, 0);
+}
+
+console.log(depthSum(data));
+console.log(depthSumWithReducer(data));
+console.log(depthSum(empty));
+console.log(depthSumWithReducer(empty));
+console.log(depthSum(allNums));
+console.log(depthSumWithReducer(allNums));
--- Mock_Interviews/version_checker.js
+++ Mock_Interviews/version_checker.js
@@ -22,13 +22,13 @@ function check(version, conditions) {
     return false;
   }
 
-  // split into substrings on delimiter ","
-  const conditionals = conditions.split(",");
+  // Remove all whitespace and split into substrings on delimiter ","
+  const conditionals = conditions.replace(/ /g, "").split(",");
   if (!conditionals.length) {
     return false;
   }
 
-  // process substrings
+  // ------------ Process Substrings ------------ //
 
   // check for "-" to exclude version number
   for (const condition of conditionals) {
@@ -41,9 +41,8 @@ function check(version, conditions) {
   }
 
   // check for exact match
-
   for (const condition of conditionals) {
-    const exactVersion = parseFloat(condition);
+    const exactVersion = parseFloat(condition); // setting to variable allows null checking
     // does not handle 0.0
     if (exactVersion && versionFloat === exactVersion) {
       return true;
@@ -51,20 +50,123 @@ function check(version, conditions) {
   }
 
   // check for conditional, like "<" or ">"
-  // check again for "="
-  // if no conditional, exact match needed
+  for (const condition of conditionals) {
+    if (condition.includes("<=")) {
+      const conditionalVersion = parseFloat(condition.slice(2));
+      if (conditionalVersion && versionFloat <= conditionalVersion) {
+        return true;
+      }
+    }
+    if (condition.includes(">=")) {
+      const conditionalVersion = parseFloat(condition.slice(2));
+      if (conditionalVersion && versionFloat >= conditionalVersion) {
+        return true;
+      }
+    }
+    if (condition.includes("<")) {
+      const conditionalVersion = parseFloat(condition.slice(1));
+      if (conditionalVersion && versionFloat < conditionalVersion) {
+        return true;
+      }
+    }
+    if (condition.includes(">")) {
+      const conditionalVersion = parseFloat(condition.slice(1));
+      if (conditionalVersion && versionFloat > conditionalVersion) {
+        return true;
+      }
+    }
+  }
 
   // look for wildcard "x"
+  for (const condition of conditionals) {
+    if (condition.includes("x")) {
+      const wildcardVersion = parseInt(condition.replace("x", "0"));
+      if (parseInt(versionFloat) === wildcardVersion) {
+        return true;
+      }
+    }
+  }
   // match the floors of version and conditional
+
+  return false;
 }
 
 // single condition is exact version, 1 == 1.0
 // - means exclusion
 // 56.x has a wildcard
 
-// check("1", "1", True)
-console.log(check("1", "1"));
-// check("1.3", "1, 1.3", True)
-console.log(check("1.3", "1, 1.3"));
-// check("1.3", "1,-1.3", False)
-console.log(check("1.3", "1, -1.3"));
+function test(expected, recieved) {
+  return `Recieved ${recieved} ${expected === recieved ? "✅" : "❌"}`;
+}
+
+console.log('check("1", "1")', "Expect true: ", test(true, check("1", "1")));
+console.log(
+  '\ncheck("1.3", "1, 1.3")',
+  "Expect true: ",
+  test(true, check("1.3", "1, 1.3"))
+); // Expect true
+console.log(
+  '\ncheck("1", "3")',
+  "Expect false: ",
+  test(false, check("1", "3"))
+); // Expect false
+console.log(
+  '\ncheck("1.7", "2, 2.8")',
+  "Expect false: ",
+  test(false, check("1.7", "2, 2.8"))
+); // Expect false
+console.log(
+  '\ncheck("1.0", ">=1")',
+  "Expect true: ",
+  test(true, check("1.0", ">=1"))
+); // Expect true
+console.log(
+  '\ncheck("1", ">=1.0")',
+  "Expect true: ",
+  test(true, check("1", ">=1.0"))
+); // Expect true
+console.log(
+  '\ncheck("1.3", ">0.2,-1.333")',
+  "Expect true: ",
+  test(true, check("1.3", ">0.2,-1.333"))
+); // Expect true
+console.log(
+  '\ncheck("1.3", ">1,-1.3")',
+  "Expect false: ",
+  test(false, check("1.3", ">1,-1.3"))
+); // Expect false
+console.log(
+  '\ncheck("2", "<1.3, 56.x")',
+  "Expect false: ",
+  test(false, check("2", "<1.3, 56.x"))
+); // Expect false
+console.log(
+  '\ncheck("1.0", "1, -1.2")',
+  "Expect true: ",
+  test(true, check("1.0", "1, -1.2"))
+); // Expect true
+console.log(
+  '\ncheck("1.3", ">0.9, -1.3")',
+  "Expect false: ",
+  test(false, check("1.3", ">0.9, -1.3"))
+); // Expect false
+console.log(
+  '\ncheck("1.3", "1.x, -1.3")',
+  "Expect false: ",
+  test(false, check("1.3", "1.x, -1.3"))
+); // Expect false
+console.log(
+  '\ncheck("1.3", "<0.2, 1.3")',
+  "Expect true: ",
+  test(true, check("1.3", "<0.2, 1.3"))
+); // Expect true
+console.log(
+  '\ncheck("1.3", "1.x")',
+  "Expect true: ",
+  test(true, check("1.3", "1.x"))
+); // Expect true
+console.log(
+  '\ncheck("2", "1.x")',
+  "Expect false: ",
+  test(false, check("2", "1.x"))
+); // Expect false
